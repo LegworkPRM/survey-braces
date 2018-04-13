467db2998ee0acdc17a01e84798d0fbebc58993f
Legwork PRM <36081238+LegworkPRM@users.noreply.github.com>
2018-04-13 09:32:39 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+// Minified for output
+// $('.yes-no').click(function(){$('.yes-no-clicked').removeClass('yes-no-clicked');$(this).addClass('yes-no-clicked')});$('.checkmark').click(function(){$(this).toggleClass('checkmark-clicked')});$('.option-container').click(function(){$('.option-container-clicked').removeClass('option-container-clicked');$(this).addClass('option-container-clicked')});$('.option-container-type-2').click(function(){$('.option-container-type-2-clicked').removeClass('option-container-type-2-clicked');$(this).addClass('option-container-type-2-clicked')})
+
+
 // When yes-no div is clicked...
 $('.yes-no').click(function() {
   // Remove the class 'yes-no-clicked' from any items that currently have it applied
